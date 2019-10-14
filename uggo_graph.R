2b6143a46b72bd40104a1cc581734ab5e5e0c41d
Kelly Money <10835147@uvu.edu>
2019-10-14 11:10:08 -0600

    Ugly

--- uggo_graph.R
+++ uggo_graph.R
@@ -38,6 +38,43 @@ uggo <-ggplot(y, aes(x=y$competitorname,y=y$sugarpercent, group = y$sugarpercent
                                     size=14, angle=45),
          axis.text.y = element_text(face="bold", color="maroon1", 
                                     size=20, angle=45))
+ 
+ 
+ #send graph to git
+ 
+ png(filename = "../../../Data_Course_Money/Assignments/uggo_graph.png")
+ 
+ uggo <-ggplot(y, aes(x=y$competitorname,y=y$sugarpercent, group = y$sugarpercent)) + 
+   geom_point(aes(shape=y$sugarpercent, color=y$sugarpercent, size=y$sugarpercent)) +
+   geom_smooth(method="lm") + scale_shape_identity()
+ 
+ 
+ uggo + theme(panel.background = element_rect(fill = "white",
+                                              colour = "yellow",
+                                              size = 0.5, linetype = "solid"),
+              panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                              colour = "yellow"), 
+              panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                              colour = "yellow")
+ ) + labs(title="Favorite Candy", subtitle="candy", color = "CompetNam",
+          y="SP", x="CP") +
+   theme(plot.background = element_rect(fill = "yellow") ) +
+   theme(text=element_text(size=24, 
+                           family= "Comic Sans MS", colour = "lawngreen")) +
+   theme(axis.text.x = element_text(face="bold", color="salmon2", 
+                                    size=15, angle=45),
+         axis.text.y = element_text(face="bold", color="maroon1", 
+                                    size=20, angle=45))
+ 
+ 
+ dev.off()
+ 
+ 
+ 
+ 
+ 
+ 
+ 
    
  warnings()
  #comic sans
